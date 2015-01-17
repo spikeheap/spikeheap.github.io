b44ef58a59fc57d28f7c811a8e4c0e532d51c2b9
Ryan Brooks <ryanbrooksis@gmail.com>
2015-01-17 15:30:27 +0000

    Updating bower config

--- Gruntfile.js
+++ Gruntfile.js
@@ -250,9 +250,9 @@ module.exports = function (grunt) {
             'images/**/*',
             'fonts/**/*',
             // Like Jekyll, exclude files & folders prefixed with an underscore.
-            '!**/_*{,/**}'
+            '!**/_*{,/**}',
             // Explicitly add any files your site needs for distribution here.
-            //'_bower_components/jquery/jquery.js',
+            '_bower_components/**/*'
             //'favicon.ico',
             //'apple-touch*.png'
           ],
