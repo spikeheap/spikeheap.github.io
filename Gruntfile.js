4bcaed49a6ed7385a19ec594f0558432300c0237
Ryan Brooks <ryanbrooksis@gmail.com>
2015-06-22 17:41:58 +0300

    Watch for style changes too

--- Gruntfile.js
+++ Gruntfile.js
@@ -31,7 +31,7 @@ module.exports = function (grunt) {
       },
       jekyll: {
         files: [
-          '<%= yeoman.app %>/**/*.{html,yml,md,mkd,markdown}',
+          '<%= yeoman.app %>/**/*.{html,scss,yml,md,mkd,markdown}',
           '!<%= yeoman.app %>/_bower_components/**/*'
         ],
         tasks: ['jekyll:server']
