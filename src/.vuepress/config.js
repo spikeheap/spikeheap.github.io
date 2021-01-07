4757dc4fd4a46a5bfcc2347b775072eeeacab901
Ryan Brooks <ryan@slatehorse.com>
2021-01-07 21:25:56 +0000

    Add mobile—friendly device width to header
    This helps with SEO and removes the horrible jankiness of an extra mm or so of left-right scroll

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -18,7 +18,8 @@ module.exports = {
   head: [
     ['meta', { name: 'theme-color', content: '#e86c12' }],
     ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
-    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }]
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
+    ['meta', { name: 'viewport', content: 'width=device-width, initial-scale=1' }]
   ],
 
   themeConfig: {
